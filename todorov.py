ce6a7cb0650102f7bd62ae43c02ae2d666ce146f
berberto <alberto.pzt@gmail.com>
2020-10-13 22:49:13 +0100

    solution desirability by svd

--- todorov.py
+++ todorov.py
@@ -1,7 +1,14 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+from fpt import meanFPT
 
+def solveSVD (A, b):
+	u, s, vh = np.linalg.svd(A, full_matrices=False)
+	c = np.dot(u.T, b)
+	w = np.linalg.solve(np.diag(s), c)
+	z = np.dot(vh.T, w)
+	return z
 
 
 # #
@@ -30,7 +37,7 @@ for k in range(N):
 	p[:,k] /= np.sum(p[:,k])	# normalize each column
 
 # define target points
-end = [N-1,N-2]
+end = np.sort(np.unique([N-2, N-1]))
 
 # "end" nodes are absorbing
 for term in end:
@@ -39,24 +46,26 @@ for term in end:
 
 # objective function parameters
 q = 1.		# cost per jump
-eps = 1.	# weight of KL
+eps = .6	# weight of KL
+tradeoff = q/eps	# only thing that matters
 
 # tilted generator
 pt = p.copy()
 for k in range(N):
 	if not k in end:
-		pt[:,k] *= np.exp(-q/eps)
+		pt[:,k] *= np.exp(-tradeoff)
 
-# solve for desirability
-Nter = len(end)
-Nrec = N - Nter
-M = pt.T
-Zrec = np.linalg.solve(M[:Nrec,:Nrec] - np.eye(Nrec), -np.sum(M[:Nrec,Nrec:], axis=1))
-Zter = np.ones(Nter)
-print(Zrec)
-print(Zter)
-Z = np.concatenate((Zrec,Zter))
-print("\ndesirability vector:    ", Z)
+# solve for desirability (at non-absorbing states)
+M = pt.T - np.eye(N)			  # tilted generator - identity
+mat = np.delete(M, end, axis=1)   # delete columns corresponding to target nodes
+vec = - np.sum(M[:,end], axis=1)  # vector implementing "boundary" conditions
+Z = np.ones(N)
+Z[np.delete(np.arange(N), end)] = solveSVD(mat, vec)
+
+# test solution:
+print("correct solution: ", np.allclose( Z - np.dot(pt.T, Z), 0), "\n")
+print("desirability vector:    ", Z, "\n")
+
 
 # controlled transition probability
 u = pt.copy()
@@ -64,7 +73,7 @@ for kp in range(N):
 	u[kp] *= Z[kp]
 for k in range(N):
 	u[:,k] /= Z[k]
-print("u correctly normalized: ", np.allclose(np.sum(u,axis=0), 1, atol=1e-15))
+print("u correctly normalized: ", np.allclose(np.sum(u,axis=0), 1, atol=1e-15), "\n")
 
 #
 #	PLOTS
@@ -83,5 +92,5 @@ ax3.set_title("Optimal tr. prob.")
 f = ax3.imshow(u)
 fig.colorbar(f, ax=ax3)
 
-plt.savefig("matrices.png")
+plt.savefig("matrices_%.1f.png"%tradeoff)
 plt.show()
