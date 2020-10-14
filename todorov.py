87d2e90bacc6a6d802623bbef8284994c0656f03
berberto <alberto.pzt@gmail.com>
2020-10-14 11:55:37 +0100

    added graph color-coded by log(desirability)

--- todorov.py
+++ todorov.py
@@ -3,6 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from fpt import meanFPT
 
+
 def solveSVD (A, b):
 	u, s, vh = np.linalg.svd(A, full_matrices=False)
 	c = np.dot(u.T, b)
@@ -14,24 +15,27 @@ def solveSVD (A, b):
 # #
 # #	define from NX graph
 # #
-# graph = nx.read_graphml("v_graph_9Oct2020.graphml")
-# adj = np.adjacency_matrix(graph).todense()
+# print("Loading NetworkX graph")
+# G = nx.read_graphml("v_graph_9Oct2020.graphml")
+# adj = nx.adjacency_matrix(G).todense()
 
 #
 #	define random (Erdos-Renyi) graph
 #
+print("Generating ER graph")
 np.random.seed(1990)
 N = 20			# number of nodes
-deg = (N-1)/5.	# average degree (a fifth of the total nodes)
+deg = 4			# average degree
 adj = np.zeros((N,N), dtype=int)
 for i in range(N):
 	for j in range(i+1,N):
-		if np.random.rand() < deg/(N-1):
+		if np.random.rand() < deg/(N-1):	
 			adj[i,j] += 1
 			adj[j,i] += 1
 
 # uncontrolled transition probability
 # transitions from a given node have equal probabilities
+print("Define random walk on graph")
 p = adj.copy().astype(float)
 for k in range(N):
 	p[:,k] /= np.sum(p[:,k])	# normalize each column
@@ -45,8 +49,9 @@ for term in end:
 	p[term,term] = 1.
 
 # objective function parameters
+print("Setting up costs and tilted generator")
 q = 1.		# cost per jump
-eps = .6	# weight of KL
+eps = 1.	# weight of KL
 tradeoff = q/eps	# only thing that matters
 
 # tilted generator
@@ -56,41 +61,58 @@ for k in range(N):
 		pt[:,k] *= np.exp(-tradeoff)
 
 # solve for desirability (at non-absorbing states)
+print("Solving for the desirability")
 M = pt.T - np.eye(N)			  # tilted generator - identity
 mat = np.delete(M, end, axis=1)   # delete columns corresponding to target nodes
 vec = - np.sum(M[:,end], axis=1)  # vector implementing "boundary" conditions
 Z = np.ones(N)
 Z[np.delete(np.arange(N), end)] = solveSVD(mat, vec)
 
-# test solution:
-print("correct solution: ", np.allclose( Z - np.dot(pt.T, Z), 0), "\n")
-print("desirability vector:    ", Z, "\n")
-
 
 # controlled transition probability
+print("Define controlled transition probabilities")
 u = pt.copy()
 for kp in range(N):
 	u[kp] *= Z[kp]
 for k in range(N):
 	u[:,k] /= Z[k]
-print("u correctly normalized: ", np.allclose(np.sum(u,axis=0), 1, atol=1e-15), "\n")
+
+
+# test solution:
+print("\nRun checks...")
+print("correct solution: ", np.allclose( Z - np.dot(pt.T, Z), 0))
+print("u correctly normalized: ", np.allclose(np.sum(u,axis=0), 1, atol=1e-15))
+print("")
+
+#	create graph from (weighted) edges -- tr. pr. matrix
+print("Create graph")
+G=nx.Graph()
+G = nx.from_numpy_matrix(u)
+pos = nx.spring_layout(G, iterations=50)
 
 #
 #	PLOTS
 #
-fig, (ax1, ax2, ax3) = plt.subplots(figsize=(13, 3), ncols=3)
+print("Plot results")
+fig, ax = plt.subplots(2,2, figsize=(8,8))
 
-ax1.set_title("Adjacency matrix")
-f = ax1.imshow(adj)
-fig.colorbar(f, ax=ax1)
+plt.sca(ax[0,0])
+ax[0,0].set_title("Adjacency matrix")
+f = ax[0,0].imshow(adj)
+# fig.colorbar(f, ax=ax[0,0])
 
-ax2.set_title("Reference tr. prob.")
-f = ax2.imshow(p)
-fig.colorbar(f, ax=ax2)
+plt.sca(ax[0,1])
+ax[0,1].set_title("Graph")
+nx.draw(G, pos, node_color=np.log(Z), node_size=80, cmap=plt.cm.Spectral, with_labels=True, font_size=6)
 
-ax3.set_title("Optimal tr. prob.")
-f = ax3.imshow(u)
-fig.colorbar(f, ax=ax3)
+plt.sca(ax[1,0])
+ax[1,0].set_title("Reference tr. prob.")
+f = ax[1,0].imshow(p)
+# fig.colorbar(f, ax=ax[1,0])
+
+plt.sca(ax[1,1])
+ax[1,1].set_title("Optimal tr. prob.")
+f = ax[1,1].imshow(u)
+# fig.colorbar(f, ax=ax[1,1])
 
 plt.savefig("matrices_%.1f.png"%tradeoff)
-plt.show()
