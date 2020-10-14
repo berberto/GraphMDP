52fcb1e43d27147f7b02978f0f541473284c55f6
berberto <alberto.pzt@gmail.com>
2020-10-14 16:35:06 +0100

    testing with true targets

--- todorov.py
+++ todorov.py
@@ -12,43 +12,44 @@ def solveSVD (A, b):
 	return z
 
 
-# #
-# #	define from NX graph
-# #
-# print("Loading NetworkX graph")
-# G = nx.read_graphml("v_graph_9Oct2020.graphml")
-# isolates = list(nx.isolates(G))
-# print("Removing isolated nodes: ", isolates)
-# G.remove_nodes_from(isolates)
-# adj = nx.adjacency_matrix(G).todense()
-# N = G.number_of_nodes()
-# print("%d nodes"%N)
-# end = [  434,  3229,  3895,  4424,  4512,  5139,  5551,  5657,  5696,
-#         5995,  6818,  7439,  7852,  8389,  9100,  9407,  9749,  9809,
-#         9825, 10028, 10722, 10969, 10979, 11087, 11421, 11455, 11569,
-#        11584, 11599, 11754, 11955, 12124, 12388, 12663, 12670, 12920,
-#        12995, 13172, 13410, 13824, 13869, 13963, 14594, 15101, 15196,
-#        15605, 15623, 15711, 16377, 16490, 16580, 16914, 16968, 16982,
-#        17130, 17141, 17197, 17294, 17296, 17499, 18183, 18603, 18787,
-#        19230, 19596, 19937, 19963, 20038, 20085, 20273, 20310, 20433,
-#        20515, 20527, 20644, 20935, 21013]
+#
+#	define from NX graph
+#
+print("Loading NetworkX graph")
+G = nx.read_graphml("v_graph_9Oct2020.graphml")
+isolates = list(nx.isolates(G))
+print("Removing isolated nodes: ", isolates)
+G.remove_nodes_from(isolates)
+adj = nx.adjacency_matrix(G).todense()
+N = G.number_of_nodes()
+print("%d nodes"%N)
+end = np.sort(np.unique(
+		[  434,  3229,  3895,  4424,  4512,  5139,  5551,  5657,  5696,
+        5995,  6818,  7439,  7852,  8389,  9100,  9407,  9749,  9809,
+        9825, 10028, 10722, 10969, 10979, 11087, 11421, 11455, 11569,
+       11584, 11599, 11754, 11955, 12124, 12388, 12663, 12670, 12920,
+       12995, 13172, 13410, 13824, 13869, 13963, 14594, 15101, 15196,
+       15605, 15623, 15711, 16377, 16490, 16580, 16914, 16968, 16982,
+       17130, 17141, 17197, 17294, 17296, 17499, 18183, 18603, 18787,
+       19230, 19596, 19937, 19963, 20038, 20085, 20273, 20310, 20433,
+       20515, 20527, 20644, 20935, 21013]))
 
 
-#
-#	define random (Erdos-Renyi) graph
-#
-print("Generating ER graph")
-np.random.seed(1990)
-N = 20			# number of nodes
-deg = 4			# average degree
-adj = np.zeros((N,N), dtype=int)
-for i in range(N):
-	for j in range(i+1,N):
-		if np.random.rand() < deg/(N-1):	
-			adj[i,j] += 1
-			adj[j,i] += 1
-# define target points
-end = np.sort(np.unique([N-2, N-1]))
+# #
+# #	define random (Erdos-Renyi) graph
+# #
+# print("Generating ER graph")
+# np.random.seed(1990)
+# N = 20			# number of nodes
+# deg = 4			# average degree
+# adj = np.zeros((N,N), dtype=int)
+# for i in range(N):
+# 	for j in range(i+1,N):
+# 		if np.random.rand() < deg/(N-1):	
+# 			adj[i,j] += 1
+# 			adj[j,i] += 1
+# # define target points
+# end = np.sort(np.unique([N-2, N-1]))
 
 
 # uncontrolled transition probability
@@ -134,7 +135,5 @@ ax[1,1].set_title("Optimal tr. prob.")
 f = ax[1,1].imshow(u)
 # fig.colorbar(f, ax=ax[1,1])
 
-# plt.savefig("%.1f_realstuff.png"%tradeoff)
-# nx.write_graphml_lxml(G,"%.1f_realstuff.graphml"%tradeoff)
-plt.savefig("N%d_deg%d_%.1f_random.png"%(N,deg,tradeoff))
-nx.write_graphml(G,"N%d_deg%d_%.1f_random.graphml"%(N,deg,tradeoff))
+plt.savefig("%.1f_realstuff.png"%tradeoff)
+nx.write_graphml(G,"%.1f_realstuff.graphml"%tradeoff)
